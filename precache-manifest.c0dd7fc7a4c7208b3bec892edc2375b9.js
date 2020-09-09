28aeb32f8447ca2464ba3f795421763d182a5828
JJ-Hall <joshhall1991@gmail.com>
2020-09-08 23:35:36 -0500

    Updates

---
+++ precache-manifest.c0dd7fc7a4c7208b3bec892edc2375b9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f1ba175faaf7a159bd662bd0a2f791",
+    "url": "/HireJoshHall/index.html"
+  },
+  {
+    "revision": "00bba4de91e9a8b362b7",
+    "url": "/HireJoshHall/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e68dd589f41cfdaac724",
+    "url": "/HireJoshHall/static/js/2.4a7d9846.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HireJoshHall/static/js/2.4a7d9846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00bba4de91e9a8b362b7",
+    "url": "/HireJoshHall/static/js/main.608b0c76.chunk.js"
+  },
+  {
+    "revision": "1e370c710bef5ffc4b23",
+    "url": "/HireJoshHall/static/js/runtime-main.6eaebbcf.js"
+  },
+  {
+    "revision": "da6630a36848215f2cbe6b7f941def30",
+    "url": "/HireJoshHall/static/media/PowerBody.da6630a3.JPG"
+  },
+  {
+    "revision": "dbbb95f01e3b5711d388edca216fb0ae",
+    "url": "/HireJoshHall/static/media/Travelsite.dbbb95f0.JPG"
+  },
+  {
+    "revision": "5f57f6045b8b936a1d23dfe1952fb59c",
+    "url": "/HireJoshHall/static/media/aboutMeButton.5f57f604.svg"
+  },
+  {
+    "revision": "a2353b04fab541a12ec0fc9d1add938e",
+    "url": "/HireJoshHall/static/media/contactButton.a2353b04.svg"
+  },
+  {
+    "revision": "2a416c3449edac25c2e65fa6accb2ce9",
+    "url": "/HireJoshHall/static/media/github.2a416c34.svg"
+  },
+  {
+    "revision": "065f72317d73b100b41f2f2b08d01813",
+    "url": "/HireJoshHall/static/media/homeButton.065f7231.svg"
+  },
+  {
+    "revision": "405ec938ada74412a13b8a78f81e4618",
+    "url": "/HireJoshHall/static/media/izuddin-h-bg.405ec938.jpg"
+  },
+  {
+    "revision": "aa4ad851c239959b000d02231e48e854",
+    "url": "/HireJoshHall/static/media/projectButton.aa4ad851.svg"
+  }
+]);
